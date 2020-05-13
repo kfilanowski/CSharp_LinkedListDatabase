24cd866af68b364db7a5059b7f458d90fd9be9be
Kevin Filanowski <kevinfilanowski@gmail.com>
2020-05-13 15:18:27 -0400

    Initial Commit. Changed two files from Java to C#.

---
+++ C_SharpLinkedListDatabase/IAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace C_SharpLinkedListDatabase
+{
+    /// <summary>
+    /// Interface class defines attribute related operations.
+    /// </summary>
+    /// Author: Kevin Filanowski
+    /// Version: 05/13/2020
+    public interface IAttribute
+    {
+        /// <summary>
+        /// Check to see if a record has an attribute containing a specific value.
+        /// </summary>
+        /// <param name="attribute">The attribute to check that value for.</param>
+        /// <param name="value">The value to check with the attribute.</param>
+        /// <returns>True if there exists an attribute with the value specified,
+        /// false otherwise.</returns>
+        public bool Check(string attribute, string value);
+
+        /// <summary>
+        /// Change the value of a specified attribute.
+        /// </summary>
+        /// <param name="attribute">The attribute to change.</param>
+        /// <param name="value">The new value to assign to the specified
+        /// attribute.</param>
+        /// <returns>True if a change was made, false otherwise.</returns>
+        public bool Change(string attribute, string value);
+
+        /// <summary>
+        /// Makes a deep copy of the employee object without using clone() or
+        /// a copy constructor.
+        /// </summary>
+        /// <returns>A deep copy of the current employee object.</returns>
+        public IAttribute makeCopy();
+    }
+}
---
+++ C_SharpLinkedListDatabase/Status.cs
@@ -0,0 +1,94 @@
+﻿using System;
+namespace C_SharpLinkedListDatabase
+{
+    /// <summary>
+    /// This class models the marital status of a person. Marital statuses
+    /// include: Married, Widowed, Divorced, and Single
+    /// </summary>
+    /// Author: Kevin Filanowski
+    /// Version: 05/13/2020
+    public class Status
+    {
+        /// <summary>
+        /// Martial Status of a person.
+        /// </summary>
+        private string _maritalStatus;
+
+        /// <summary>
+        /// Property defining set and getting methods for the martial status.
+        /// </summary>
+        private string MaritalStatus
+        {
+            get
+            {
+                return _maritalStatus;
+            }
+            set
+            {
+                // Assign the full marital status word.
+                switch (value)
+                {
+                    case "Married":
+                    case "married":
+                    case "m":
+                    case "M": _maritalStatus = "Married"; break;
+                    case "widowed":
+                    case "Widowed":
+                    case "w":
+                    case "W": _maritalStatus = "Widowed"; break;
+                    case "divorced":
+                    case "Divorced":
+                    case "d":
+                    case "D": _maritalStatus = "Divorced"; break;
+                    case "Single":
+                    case "single":
+                    case "s":
+                    case "S": _maritalStatus = "Single"; break;
+                    default:
+                        {
+                            throw new ArgumentException("Invalid Marital Status, "
+                                    + "Must be Married, Widowed, Divorced, or Single.");
+                        }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Constructor to set the marital status of the person.
+        /// </summary>
+        /// <param name="maritalStatus"></param>
+        public Status(string maritalStatus)
+        {
+            _maritalStatus = maritalStatus;
+        }
+
+        /// <summary>
+        /// Compares two Status objects by their marital status. If the marital
+        /// status is the same, then they are considered equal.
+        /// </summary>
+        /// <param name="obj">The object to compare with.</param>
+        /// <returns>True if the two objects are equal, false if they are not.</returns>
+        public override bool Equals(object obj)
+        {
+            // Check if obj is a Status object, then cast and compare it.
+            if (obj is Status o)
+            {
+                // If the fields are the same, then they are equal.
+                if (o.MaritalStatus.Equals(MaritalStatus))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the marital status of the associated Status object.
+        /// </summary>
+        /// <returns>The marital status of the person.</returns>
+        public override string ToString()
+        {
+            return MaritalStatus;
+        }
+    }
+}
